389343fe0e88c4f9eb1483880d2c0f7f34820283
njlane314 <njlane314@gmail.com>
2026-05-08 20:57:53 +0100

    Add 2227B solution stub

---
+++ solutions/B.2227.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+void solve(istream& in = cin, ostream& out = cout) {
+    (void)in;
+    (void)out;
+
+    int n;
+    in >> n;
+
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+    return 0;
+}
