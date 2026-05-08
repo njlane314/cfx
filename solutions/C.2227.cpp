b2aadd28e6315133f6410fa2789ced0125955623
njlane314 <njlane314@gmail.com>
2026-05-08 23:21:05 +0100

    Solve C.2227

--- solutions/C.2227.cpp
+++ solutions/C.2227.cpp
@@ -12,7 +12,47 @@ using pll = pair<ll, ll>;
 #define all(x) begin(x), end(x)
 #define rall(x) rbegin(x), rend(x)
 
-void solve() {}
+void solve() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<ll> two;
+        vector<ll> neutral;
+        vector<ll> three;
+        vector<ll> six;
+
+        for (int i = 0; i < n; i++) {
+            ll x;
+            cin >> x;
+
+            if (x % 6 == 0) {
+                six.push_back(x);
+            } else if (x % 2 == 0) {
+                two.push_back(x);
+            } else if (x % 3 == 0) {
+                three.push_back(x);
+            } else {
+                neutral.push_back(x);
+            }
+        }
+
+        auto print = [](const vector<ll>& v) {
+            for (ll x : v) {
+                cout << x << ' ';
+            }
+        };
+
+        print(two);
+        print(neutral);
+        print(three);
+        print(six);
+        cout << '\n';
+    }
+}
 
 int main() {
     ios::sync_with_stdio(false);
